cbdaf46e05f64c078c18fa6f1dcf8b7800c23108
Stuart Herbert <stuart@stuartherbert.com>
2024-03-02 12:38:32 +0000

    Fix: reverse the code-rot

---
+++ src/ParsedURL/ParsedURL.ts
@@ -0,0 +1,98 @@
+//
+// Copyright (c) 2020-present Ganbaro Digital Ltd.
+//
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are
+// met:
+//
+// 1. Redistributions of source code must retain the above copyright
+//    notice, this list of conditions and the following disclaimer.
+// 2. Redistributions in binary form must reproduce the above copyright
+//    notice, this list of conditions and the following disclaimer in the
+//    documentation and/or other materials provided with the
+//    distribution.
+// 3. Neither the names of the copyright holders nor the names of its
+//    contributors may be used to endorse or promote products derived
+//    from this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+// A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+// HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+// SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+// LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+// DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+// THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+// OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//
+
+import type { IpPort } from "@safelytyped/ip-port";
+import url from "url";
+
+/**
+ * `ParsedURL` holds a breakdown of a URL, using terms from the WHATWG
+ * specification.
+ *
+ * NOTE that we do support the `username` and `password` fields, but
+ * you shouldn't use them in new code. These are deprecated by RFC 3986,
+ * and may not be supported by your choice of browser AND/OR your destination.
+ *
+ * NOTE that we do *NOT support protocol-relative URLs here, because
+ * the underlying NodeJS URL doesn't support them.
+ *
+ * @category ParsedURL
+ */
+export interface ParsedURL {
+    /**
+     * `protocol` holds the network scheme to use (eg 'http' or 'https').
+     */
+    protocol: string;
+
+    /**
+     * `username` holds the username to authenticate as (eg `git`).
+     */
+    username?: string;
+
+    /**
+     * `password` holds the password to authenticate with.
+     */
+    password?: string;
+
+    /**
+     * `hostname` is the server where the remote data is hosted.
+     */
+    hostname: string;
+
+    /**
+     * `port` is the IP port number to connect to on the remote hostname.
+     *
+     * A value of `undefined` means to use the default IP port number
+     * for the given `protocol`.
+     */
+    port?: IpPort;
+
+    /**
+     * `pathname` is the query path portion of the URL.
+     */
+    pathname: string;
+
+    /**
+     * `search` holds the query string portion of the URL.
+     */
+    search?: string;
+
+    /**
+     * `searchParams` holds the query string portion of the URL,
+     * as a data bag.
+     */
+    searchParams?: url.URLSearchParams;
+
+    /**
+     * `hash` holds the #fragment section of the URL, if present.
+     */
+    hash?: string;
+}
---
+++ src/URL/mustBeURLData.ts
@@ -0,0 +1,80 @@
+//
+// Copyright (c) 2020-present Ganbaro Digital Ltd.
+//
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are
+// met:
+//
+// 1. Redistributions of source code must retain the above copyright
+//    notice, this list of conditions and the following disclaimer.
+// 2. Redistributions in binary form must reproduce the above copyright
+//    notice, this list of conditions and the following disclaimer in the
+//    documentation and/or other materials provided with the
+//    distribution.
+// 3. Neither the names of the copyright holders nor the names of its
+//    contributors may be used to endorse or promote products derived
+//    from this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+// A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+// HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+// SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+// LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+// DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+// THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+// OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//
+import {
+    type DataPath,
+    DEFAULT_DATA_PATH,
+    mustBe,
+    type OnError,
+    THROW_THE_ERROR,
+} from "@safelytyped/core-types";
+
+import { validateURLData } from "./validateURLData";
+
+/**
+ * `mustBeURLData()` is a data guard. Use it to ensure that the given
+ * `input` value can be used as a URL.
+ *
+ * NOTE: for partial URLS (e.g. fragments), and for relative URLs,
+ * you need to set `base` too. The final URL will be evaluated by
+ * combining `base` and `input`.
+ *
+ * @param input
+ * The value to ensure.
+ * @param base
+ * If `input` is not a full URL, set `base` to a full URL.
+ * @param onError
+ * If `input` fails validation, we'll call your `onError` handler with an
+ * `AppError` to explain why.
+ * @param path
+ * Where are you in validating your nested structure? Use
+ * {@link DEFAULT_DATA_PATH} if you are not in a nested data structure.
+ * @returns
+ * - `input` converted to a `URL` on success
+ *
+ * @category URL
+ */
+export function mustBeURLData(
+    input: unknown,
+    {
+        base,
+        onError = THROW_THE_ERROR,
+        path = DEFAULT_DATA_PATH
+    }: {
+        base?: string,
+        onError?: OnError,
+        path?: DataPath
+    } = {}
+): URL {
+    return mustBe(input, { onError })
+        .next((x) => validateURLData(path, x, { base }))
+        .value();
+}
